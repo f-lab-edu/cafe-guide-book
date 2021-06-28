b1a9d486ae8b91e9f809adbfecc40d28b9bd7378
jsj3282 <jeongseonju15@gmail.com>
2021-06-29 00:56:15 +0900

    project settings

---
+++ src/test/java/com/flab/cafeguidebook/CafeguidebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.cafeguidebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeguidebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
