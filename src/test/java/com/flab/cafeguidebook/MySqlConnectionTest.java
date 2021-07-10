a8b19e133607ecc5bbe47ea0ca812af7574f1856
김민성 <yssj2049@gmail.com>
2021-07-11 00:17:17 +0900

    Update println to java Logger
    Make corrections based on feedback.

--- src/test/java/com/flab/cafeguidebook/MySqlConnectionTest.java
+++ src/test/java/com/flab/cafeguidebook/MySqlConnectionTest.java
@@ -2,6 +2,7 @@ package com.flab.cafeguidebook;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 @SpringBootTest
 public class MySqlConnectionTest {
 
-  private static final String DRIVER = "com.mysql.jdbc.Drive";
+  private static final String DRIVER = "com.mysql.jdbc.Driver";
   private static final String URL = "jdbc:mysql://localhost:3306?serverTimezone=UTC&characterEncoding=UTF-8";
   private static final String USER = "root";
   private static final String PW = "Cafe1234!";
@@ -17,10 +18,12 @@ public class MySqlConnectionTest {
   @Test
   public void testConnection() throws Exception{
 
+    final Logger LOG = Logger.getGlobal();
     Class.forName(DRIVER);
 
     try(Connection con = DriverManager.getConnection(URL, USER, PW)){
       System.out.println(con);
+      LOG.info(con.toString());
     }catch(Exception e) {
       fail(e);
     }
