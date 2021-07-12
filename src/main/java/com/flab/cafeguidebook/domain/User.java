9b882ed183bc527b00ced2cf3a99fdcabf0eb489
김민성 <yssj2049@gmail.com>
2021-07-12 23:41:56 +0900

    Create User.java

---
+++ src/main/java/com/flab/cafeguidebook/domain/User.java
@@ -0,0 +1,73 @@
+package com.flab.cafeguidebook.domain;
+
+import com.flab.cafeguidebook.enumeration.UserStatus;
+import com.flab.cafeguidebook.enumeration.UserType;
+
+public class User {
+
+  private String email;
+  private String password;
+  private String name;
+  private String phone;
+  private String address;
+  private UserType userType;
+
+  public User(String email, String password, String name, String phone, String address,
+      UserType userType, UserStatus userStatus) {
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.userType = userType;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public UserType getUserType() {
+    return userType;
+  }
+
+  public void setUserType(UserType userType) {
+    this.userType = userType;
+  }
+
+}
