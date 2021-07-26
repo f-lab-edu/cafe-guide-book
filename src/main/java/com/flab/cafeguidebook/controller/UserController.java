ee39cca09c5583631d2ba0cb62f14209070f5c9d
kimminseong <ms@aithe.io>
2021-07-26 15:27:58 +0900

    Add @Autowired in UserController

--- src/main/java/com/flab/cafeguidebook/controller/UserController.java
+++ src/main/java/com/flab/cafeguidebook/controller/UserController.java
@@ -2,8 +2,13 @@ package com.flab.cafeguidebook.controller;
 
 import com.flab.cafeguidebook.dto.UserDTO;
 import com.flab.cafeguidebook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -11,6 +16,7 @@ public class UserController {
 
   private UserService userService;
 
+  @Autowired
   public UserController(UserService userService) {
     this.userService = userService;
   }
@@ -18,7 +24,7 @@ public class UserController {
   public UserController() {
   }
 
-  @PostMapping("/signUp")
+  @PostMapping(value = "/signUp")
   @ResponseStatus(HttpStatus.CREATED)
   public void signUp(@RequestBody UserDTO userDTO) {
     userService.signUp(userDTO);
