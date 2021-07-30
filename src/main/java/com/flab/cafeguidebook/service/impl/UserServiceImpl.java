ca8a78d73b419a68549db41ce419ea11b92f24bd
김민성 <yssj2049@gmail.com>
2021-07-30 23:46:12 +0900

    Add for sign in feature

--- src/main/java/com/flab/cafeguidebook/service/impl/UserServiceImpl.java
+++ src/main/java/com/flab/cafeguidebook/service/impl/UserServiceImpl.java
@@ -1,18 +1,23 @@
 package com.flab.cafeguidebook.service.impl;
 
 import com.flab.cafeguidebook.dto.UserDTO;
+import com.flab.cafeguidebook.exception.UserNotFoundException;
 import com.flab.cafeguidebook.mapper.UserMapper;
 import com.flab.cafeguidebook.service.UserService;
 import com.flab.cafeguidebook.util.HashingUtil;
+import com.flab.cafeguidebook.util.SessionKeys;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
   private final UserMapper userMapper;
+  private final HttpSession httpSession;
 
-  public UserServiceImpl(UserMapper userMapper) {
+  public UserServiceImpl(UserMapper userMapper, HttpSession httpSession) {
     this.userMapper = userMapper;
+    this.httpSession = httpSession;
   }
 
   // TODO : 이메일 중복체크 기능 추가 예정
@@ -20,5 +25,17 @@ public class UserServiceImpl implements UserService {
   public boolean signUp(UserDTO userDTO) {
     userDTO.setPassword(HashingUtil.sha256Hashing(userDTO.getPassword()));
     return (userMapper.insertUser(userDTO) == 1);
+  }
+
+  @Override
+  public void signIn(String email, String password) {
+    UserDTO loginedUser = userMapper
+        .selectUserByEmailAndPassword(email, HashingUtil.sha256Hashing(password));
+
+    if (loginedUser == null) {
+      throw new UserNotFoundException("이메일 혹은 비밀번호가 잘못되었습니다.");
+    }
+
+    httpSession.setAttribute(SessionKeys.USER_EMAIL, loginedUser.getEmail());
   }
 }
