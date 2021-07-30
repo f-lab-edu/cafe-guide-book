0b3cb2bf010db0b2a3993218bb1cddc3468355ac
김민성 <yssj2049@gmail.com>
2021-07-30 23:45:14 +0900

    Create UserNotFoundException.java

---
+++ src/main/java/com/flab/cafeguidebook/exception/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package com.flab.cafeguidebook.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+  public UserNotFoundException() {
+  }
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+
+  public UserNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
