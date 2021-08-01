5b3363677113899a56f72f3f37e731659eb28d6a
김민성 <yssj2049@gmail.com>
2021-08-02 00:18:11 +0900

    Revert "Add logout test code"
    This reverts commit 06da8615ca8fa2608af6a117fe32d24c040218a4.

--- src/test/java/com/flab/cafeguidebook/service/UserServiceTest.java
+++ src/test/java/com/flab/cafeguidebook/service/UserServiceTest.java
@@ -1,7 +1,6 @@
 package com.flab.cafeguidebook.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
