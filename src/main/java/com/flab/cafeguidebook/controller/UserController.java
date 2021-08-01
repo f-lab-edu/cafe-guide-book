1545b8869c37ce66380d358895222559e39fbc9f
김민성 <yssj2049@gmail.com>
2021-08-02 00:28:37 +0900

    Add logout feature

--- src/main/java/com/flab/cafeguidebook/controller/UserController.java
+++ src/main/java/com/flab/cafeguidebook/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,4 +43,10 @@ public class UserController {
   public void signIn(String email, String password) {
     userService.signIn(email, password);
   }
+
+  @GetMapping(value = "/logout")
+  public void logout() {
+    userService.logout();
+  }
+
 }
