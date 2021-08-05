3330ed3264041260ae645fc6b49e89f18d3c5c2f
김민성 <yssj2049@gmail.com>
2021-08-06 00:18:25 +0900

    Add test code for get user information feature

--- src/test/java/com/flab/cafeguidebook/extension/UserFixtureProvider.java
+++ src/test/java/com/flab/cafeguidebook/extension/UserFixtureProvider.java
@@ -1,7 +1,6 @@
 package com.flab.cafeguidebook.extension;
 
 import com.flab.cafeguidebook.domain.User;
-import com.flab.cafeguidebook.enumeration.UserType;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
@@ -24,7 +23,6 @@ public class UserFixtureProvider implements ParameterResolver {
         .name("김민성")
         .phone("010-8358-2049")
         .address("경기도 화성시 호수공원")
-        .userType(UserType.USER)
         .build();
   }
 }
