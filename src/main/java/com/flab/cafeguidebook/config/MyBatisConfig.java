9680abe0295c140e3e10498dd6a7aa50a27b53af
jsj3282 <jeongseonju15@gmail.com>
2021-08-21 01:07:48 +0900

    HikariCP 연동하기
    indented 2칸 적용

--- src/main/java/com/flab/cafeguidebook/config/MyBatisConfig.java
+++ src/main/java/com/flab/cafeguidebook/config/MyBatisConfig.java
@@ -1,11 +1,14 @@
 package com.flab.cafeguidebook.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,23 +19,34 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class MyBatisConfig {
 
-    @Autowired
-    private ApplicationContext applicationContext;
+  @Autowired
+  private ApplicationContext applicationContext;
 
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        sessionFactory
-            .setMapperLocations(applicationContext.getResources("classpath:/mybatis/mapper/*.xml"));
-        return sessionFactory.getObject();
-    }
+  @Bean
+  @ConfigurationProperties(prefix = "spring.datasource.hikari")
+  public HikariConfig hikariConfig() {
+    return new HikariConfig();
+  }
 
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory)
-        throws Exception {
-        final SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
-        return sqlSessionTemplate;
-    }
+  @Bean
+  public DataSource dataSource() {
+    return new HikariDataSource(hikariConfig());
+  }
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+    final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+    sessionFactory.setDataSource(dataSource);
+    sessionFactory
+        .setMapperLocations(applicationContext.getResources("classpath:/mybatis/mapper/*.xml"));
+    return sessionFactory.getObject();
+  }
+
+  @Bean
+  public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory)
+      throws Exception {
+    final SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
+    return sqlSessionTemplate;
+  }
 
 }
