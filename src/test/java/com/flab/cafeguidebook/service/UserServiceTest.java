81c7869293fb2d5c11d5ea4d23989de6cf6ff202
김민성 <yssj2049@gmail.com>
2021-08-28 00:50:52 +0900

    Add check email is duplicated unit test

--- src/test/java/com/flab/cafeguidebook/service/UserServiceTest.java
+++ src/test/java/com/flab/cafeguidebook/service/UserServiceTest.java
@@ -1,21 +1,23 @@
 package com.flab.cafeguidebook.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.flab.cafeguidebook.dto.UserDTO;
 import com.flab.cafeguidebook.exception.UserNotFoundException;
 import com.flab.cafeguidebook.fixture.UserDTOFixtureProvider;
 import com.flab.cafeguidebook.mapper.UserMapper;
-import com.flab.cafeguidebook.service.impl.UserServiceImpl;
 import com.flab.cafeguidebook.util.HashingUtil;
 import com.flab.cafeguidebook.util.SessionKeys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -24,38 +26,35 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 class UserServiceTest {
 
-    @Autowired
-    private UserMapper userMapper;
+  @Mock
+  private UserMapper userMapper;
 
-    private MockHttpSession mockHttpSession;
+  private MockHttpSession mockHttpSession;
 
-    @Autowired
-    private UserServiceImpl userService;
+  @Mock
+  private UserService userService;
 
-    @Test
-    @DisplayName("이메일, 비밀번호, 이름, 휴대폰번호, 주소, 유저타입이 입력된 경우 회원가입 성공")
-    public void signUpTestSuccess(UserDTO user) {
+  @Test
+  @DisplayName("이메일, 비밀번호, 이름, 휴대폰번호, 주소, 유저타입이 입력된 경우 회원가입 성공")
+  public void signUpTestSuccess(UserDTO user) {
+    String originalPassword = user.getPassword();
 
-        String originalPassword = user.getPassword();
+    boolean isSuccess = userService.signUp(user);
 
-        boolean isSuccess = userService.signUp(user);
-
-        assertEquals(
-            HashingUtil.sha256Hashing(originalPassword),
-            userMapper.selectUserByEmail(user.getEmail()).getPassword()
-        );
-        assertTrue(isSuccess);
-    }
+    assertEquals(
+        HashingUtil.sha256Hashing(originalPassword),
+        userMapper.selectUserByEmail(user.getEmail()).getPassword()
+    );
+    assertTrue(isSuccess);
+  }
 
   @Test
   @DisplayName("이메일, 비밀번호, 이름, 휴대폰번호, 주소, 유저타입이 입력된 경우 회원가입 성공")
   public void signInTestSuccess(UserDTO user) {
-
     userService.signIn(user.getEmail(), user.getPassword());
 
     assertThat(mockHttpSession.getAttribute(SessionKeys.USER_EMAIL)).isNotNull();
     assertThat(mockHttpSession.getAttribute(SessionKeys.USER_EMAIL)).isEqualTo(user.getEmail());
-
   }
 
   @Test
@@ -74,4 +73,15 @@ class UserServiceTest {
     });
   }
 
+  @Test
+  @DisplayName("이메일")
+  public void isDuplicatedEmailTrue(UserDTO user) {
+    when(userMapper.isEmailDuplicated(user.getEmail())).thenReturn(1);
+    when(userMapper.isEmailDuplicated(user.getEmail() + "no-duplicated")).thenReturn(0);
+
+    assertTrue(userService.isDuplicatedEmail(user.getEmail()));
+    assertFalse(userService.isDuplicatedEmail(user.getEmail() + "no-duplicated"));
+
+    verify(userMapper).isEmailDuplicated(user.getEmail());
+  }
 }
