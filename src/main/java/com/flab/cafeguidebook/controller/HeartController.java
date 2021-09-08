15e5a7fa47489fa02c57186b47afd93dacf23d26
김민성 <yssj2049@gmail.com>
2021-09-09 00:28:17 +0900

    [#67] feat : 유저가 자신이 맘에 드는 카페에 좋아요 기능 구현

--- src/main/java/com/flab/cafeguidebook/controller/HeartController.java
+++ src/main/java/com/flab/cafeguidebook/controller/HeartController.java
@@ -4,12 +4,14 @@ import com.flab.cafeguidebook.annotation.SignInCheck;
 import com.flab.cafeguidebook.service.HeartService;
 import com.flab.cafeguidebook.util.SessionKeys;
 import javax.servlet.http.HttpSession;
+import com.flab.cafeguidebook.service.HeartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/heart")
