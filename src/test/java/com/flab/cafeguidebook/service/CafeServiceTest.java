88ae34e7c4c286de0ea7b56725b4714c4d0838c5
kimminseong <ms@aithe.io>
2021-09-10 11:04:39 +0900

    [#30] feat : 어드민이 신규 Cafe에 대한 승인 및 반려 기능에 대한 단위테스트 추가

--- src/test/java/com/flab/cafeguidebook/service/CafeServiceTest.java
+++ src/test/java/com/flab/cafeguidebook/service/CafeServiceTest.java
@@ -1,5 +1,7 @@
 package com.flab.cafeguidebook.service;
 
+import static com.flab.cafeguidebook.enumeration.CafeRegistration.APPROVAL;
+import static com.flab.cafeguidebook.enumeration.CafeRegistration.DENY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
@@ -27,5 +29,21 @@ public class CafeServiceTest {
   public void addCafe(CafeDTO testCafeDTO) {
     given(cafeMapper.insertCafe(testCafeDTO)).willReturn(1);
     assertThat(cafeService.addCafe(testCafeDTO)).isEqualTo(true);
+  }
+
+  @Test
+  public void approveRegistrationCafeSuccess() {
+    given(cafeMapper.updateRegistration(33333L, APPROVAL))
+        .willReturn(1);
+
+    assertThat(cafeService.approveRegistration(33333L)).isEqualTo(true);
+  }
+
+  @Test
+  public void denyRegistrationCafeSuccess() {
+    given(cafeMapper.updateRegistration(33333L, DENY))
+        .willReturn(1);
+
+    assertThat(cafeService.denyRegistration(33333L)).isEqualTo(true);
   }
 }
