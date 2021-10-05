60e1e363eed0e2b46a86424b2381641fcc10a9ea
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-10-05 16:22:29 +0900

    [#132] fix : 필요없는 import 문 제거

--- src/main/java/com/flab/cafeguidebook/CafeguidebookApplication.java
+++ src/main/java/com/flab/cafeguidebook/CafeguidebookApplication.java
@@ -2,8 +2,6 @@ package com.flab.cafeguidebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class CafeguidebookApplication {
